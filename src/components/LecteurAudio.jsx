0ff1f9f8eae3e0bc478458fd60cab333eff7f601
Pawnee <pawneedefize@hotmail.com>
2026-08-20 16:17:29 +0200

    feat: Ajout des bouton suivant et précédent qui boucle

--- src/components/LecteurAudio.jsx
+++ src/components/LecteurAudio.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import ensemble from "../assets/audio/01-ensemble.mp3"
 import verite from "../assets/audio/03-verite.mp3"
 import neige_eternelle from "../assets/audio/Neige-eternelle-4.mp3"
@@ -7,8 +8,25 @@ import voir_en_soi from "../assets/audio/Pauline-Martos-02-Voir-en-soi.mp3"
 import croire from "../assets/audio/Projet-Croire-02.mp3"
 
 function LecteurAudio() {
+  const playlist = [
+    { titre: "Ensemble", fichier: ensemble },
+    { titre: "Vérité", fichier: verite },
+    { titre: "Neige éternelle", fichier: neige_eternelle },
+    { titre: "Ombre et Lumière", fichier: ombre_lumiere },
+    { titre: "Opus 20 n°100", fichier: opus_100 },
+    { titre: "Voir en soi", fichier: voir_en_soi },
+    { titre: "Croire", fichier: croire },
+  ]
+
+  const [indexStart, setIndexStart] = useState(0)
+
     return(
-      <audio className="bottom-0 left-0 w-full fixed" controls src={neige_eternelle} />
+      <div className="bottom-0 left-0 w-full fixed">
+        <audio controls src={playlist[indexStart].fichier} />
+        <button onClick={() => setIndexStart((indexStart - 1 + playlist.length) % playlist.length)}>Précédent</button>
+        <button onClick={() => setIndexStart((indexStart + 1) %playlist.length)}>Suivant</button>
+        <p>{playlist[indexStart].titre}</p>
+      </div>
     )
 }
 
