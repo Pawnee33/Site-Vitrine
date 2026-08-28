c6c722b28cbddd5dbbe54e8a54a743c878940033
Pawnee33 <pawneedefize@hotmail.com>
2026-08-28 10:15:34 +0200

    feat: intègre la vidéo locale compressée dans la section Accroche

--- src/components/Accroche.jsx
+++ src/components/Accroche.jsx
@@ -1,4 +1,5 @@
 import Portrait from "../assets/images/DSCF8139.jpg"
+import maVideo from "../assets/video/Vitrine_compresse.mp4"
 
 
 function Accroche() {
@@ -8,14 +9,7 @@ function Accroche() {
             <p className="font-cursive font-normal text-black text-5xl">Entre Ombre & Lumière, là où mes paroles s'arrêtent, ma musique continue de vivre.</p>
         </div>
         <div className="relative w-full aspect-video object-cover">
-          <iframe
-            className="w-full h-full"
-            src="https://www.youtube.com/embed/42J3Lvkz-j8?autoplay=1&mute=1&loop=1&playlist=42J3Lvkz-j8&controls=0&rel=0"
-            title="Bande démo Pauline Defize"
-            frameBorder="0"
-            allow="autoplay; encrypted-media; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <video className="w-full aspect-video object-cover" src={maVideo} autoPlay muted loop></video>
           <div className="absolute inset-0"></div>
         </div>
       </section>
