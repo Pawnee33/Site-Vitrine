8fd1c758d4dd22bff8712665e156190e6f47e7c2
Pawnee33 <pawneedefize@hotmail.com>
2026-08-28 11:13:30 +0200

    feat: ajout d'ornement sous le titre

--- src/components/Accroche.jsx
+++ src/components/Accroche.jsx
@@ -1,12 +1,13 @@
-import Portrait from "../assets/images/DSCF8139.jpg"
+import ligne_ornement from "../assets/images/sous-titre2.png"
 import maVideo from "../assets/video/Vitrine_compresse.mp4"
 
 
 function Accroche() {
     return(
       <section>
-        <div className="flex justify-center items-center bg-sky-100 h-64">
-            <p className="font-cursive font-normal text-black text-5xl">Entre Ombre & Lumière, là où mes paroles s'arrêtent, ma musique continue de vivre.</p>
+        <div className="flex flex-col items-center bg-sky-100 h-72 relative">
+            <p className="font-cursive font-normal text-black text-5xl mt-25">Entre Ombre & Lumière, là où mes paroles s'arrêtent, ma musique continue de vivre.</p>
+            <img className="w-96 absolute pt-18" src={ligne_ornement}  alt="ornement décoratif" />
         </div>
         <div className="relative w-full aspect-video object-cover">
           <video className="w-full aspect-video object-cover" src={maVideo} autoPlay muted loop></video>
