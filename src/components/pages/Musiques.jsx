00fc0a52318d27148ead8fb546475ffaced48374
Pawnee33 <pawneedefize@hotmail.com>
2026-09-07 17:56:13 +0200

    feat: ajout de la page musique avec l'album et le lecteur audio

--- src/components/pages/Musiques.jsx
+++ src/components/pages/Musiques.jsx
@@ -10,8 +10,10 @@ import pochette_album from "../../assets/images/Pauline_hero.jpg"
 import { useState, useEffect } from "react"
 import WavesurferPlayer from "@wavesurfer/react"
 import { Play, Pause, SkipBack, SkipForward } from "lucide-react"
+import useOnScreen from "../../hooks/useOnScreen"
 
 function Musiques() {
+  const [cardRef, cardVisible] = useOnScreen()
   const [selectionMorceaux, setSelectionMorceaux] = useState(0);
   const [wavesurfer, setWavesurfer] = useState(null);
   const [enLecture, setEnLecture] = useState(false);
@@ -50,7 +52,7 @@ function Musiques() {
       className="bg-nuit/98 pt-36 pb-36">
       <div className="flex flex-col justify-center items-center m-6">
         <h1 className="font-musique2 text-4xl text-white">Album : <span className="text-amber-400">{OmbreetLumiere.titre}</span></h1>
-        <div className="flex flex-row items-center bg-gray-900 w-max rounded-2xl shadow-amber-900 shadow-lg m-10 gap-6">
+        <div className={`flex flex-row items-center bg-gray-900 w-max rounded-2xl shadow-amber-900 shadow-lg m-10 gap-6 transition-all duration-700 ${cardVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-52"}`} ref={cardRef}>
           <img className="aspect-square object-cover rounded-xl max-w-lg" src={OmbreetLumiere.image} alt="Pauline au piano album" />
           <div className="flex flex-col gap-2 p-5">
             <p className="font-musique2 text-xl text-white">Artiste: <span className="text-amber-400">{OmbreetLumiere.artiste}</span></p>
